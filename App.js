5c803a57364203d54191ad9f385220cb128d537a
thiago morales <thiagwwws@hotmail.com>
2021-08-23 21:15:46 -0300

    coloquei admin

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Detalhes">
+      <Stack.Navigator initialRouteName="Admin">
         <Stack.Screen
         name="Home"
         component={Home}
